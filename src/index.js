7455ba5c0ee5b7b51541b97d8d241b0551988a2b
DangerDanny <Dfbejarano4@gmail.com>
2021-09-20 21:36:29 -0400

    0.2.2

--- src/index.js
+++ src/index.js
@@ -1,40 +1,9 @@
 import * as bootstrap from "bootstrap";
+import localStartCount from "./scripts/localStartCount";
+import addOne from "./scripts/addOne";
 let theParent = document.querySelector("#taskList");
 global.totalCount = Number(document.getElementById("total").innerHTML);
 theParent.addEventListener("click", addOne, false);
-
-function localStartCount(cID, C) {
-  if (localStorage.getItem(cID) === null) {
-    return;
-  } else {
-    if (cID === "total") {
-      document.getElementById(cID).innerHTML = C;
-      totalCount = C;
-    } else {
-      document.getElementById(cID).firstElementChild.innerHTML = C;
-    }
-    localStorage.setItem(cID, C);
-  }
-}
-
-function addOne(t) {
-  if (t.target !== t.currentTarget) {
-    let clickedItem = t.target.id;
-    let count = Number(
-      document.getElementById(clickedItem).firstElementChild.innerHTML
-    );
-    count++;
-    document.getElementById(clickedItem).firstElementChild.innerHTML = count;
-    localStorage.setItem(clickedItem, count);
-    if (clickedItem === "Goal Tasks") {
-      totalCount += 2;
-    } else {
-      totalCount++;
-    }
-    document.getElementById("total").innerHTML = totalCount;
-    localStorage.setItem("total", totalCount);
-  }
-}
 
 localStartCount("total", Number(localStorage.getItem("total")));
 const allButtons = document.querySelectorAll("button");
