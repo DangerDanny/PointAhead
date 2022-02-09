496769cdb155fd75349ac56a3d7179dd038804a7
DangerDanny <Dfbejarano4@gmail.com>
2022-02-09 11:49:02 -0500

    Corrected Js for new button

--- src/scripts/addOne.js
+++ src/scripts/addOne.js
@@ -7,7 +7,7 @@ const addOne = (t) => {
     count++;
     document.getElementById(clickedItem).firstElementChild.innerHTML = count;
     localStorage.setItem(clickedItem, count);
-    if (clickedItem === "Goal Tasks" || clickedItem === "Coding Videos") {
+    if (clickedItem === "Goal Tasks" || clickedItem === "Coding Tasks") {
       global.totalCount += 2;
     } else {
       global.totalCount++;
